de56fe8489fc0fd73bfd22e8952227815f4e47d2
Kamila <kmilaporto1991@gmail.com>
2019-05-29 22:43:12 -0300

    adiciona controllers e templates para especialidades

---
+++ src/main/java/br/edu/ifpb/pweb2/projeto/simpleeventFKR/controllers/EspecialidadeController.java
@@ -0,0 +1,39 @@
+package br.edu.ifpb.pweb2.projeto.simpleeventFKR.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifpb.pweb2.projeto.simpleeventFKR.dao.EspecialidadeDAO;
+import br.edu.ifpb.pweb2.projeto.simpleeventFKR.model.Especialidade;
+
+@Controller
+@RequestMapping("/especialidades")
+public class EspecialidadeController {
+	
+	@Autowired
+	private EspecialidadeDAO dao;
+	
+	@RequestMapping("")
+	public ModelAndView list() {
+		ModelAndView model = new ModelAndView("especialidades");
+		List<Especialidade> espec = dao.findAll();
+		model.addObject("especialidades", espec);
+		return model;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public String save(Especialidade especialidade) {
+		dao.save(especialidade);
+		return "redirect:especialidades";
+	}
+	
+	@RequestMapping("/form")
+	public String form() {
+		return "especialidadeForm";
+	}
+}
