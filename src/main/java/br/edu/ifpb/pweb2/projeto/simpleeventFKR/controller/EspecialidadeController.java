65d8d1aecba740d106e85c356e6e0b00e49da469
Kamila <kmilaporto1991@gmail.com>
2019-06-01 14:25:07 -0300

    Adiciona funcoes de editar e apagar no controller de especialidades

--- src/main/java/br/edu/ifpb/pweb2/projeto/simpleeventFKR/controller/EspecialidadeController.java
+++ src/main/java/br/edu/ifpb/pweb2/projeto/simpleeventFKR/controller/EspecialidadeController.java
@@ -1,9 +1,11 @@
 package br.edu.ifpb.pweb2.projeto.simpleeventFKR.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,24 +18,51 @@ import br.edu.ifpb.pweb2.projeto.simpleeventFKR.model.Especialidade;
 public class EspecialidadeController {
 	
 	@Autowired
-	private EspecialidadeDAO dao;
-	
-	@RequestMapping("")
-	public ModelAndView list() {
-		ModelAndView model = new ModelAndView("especialidade/list");
-		List<Especialidade> espec = dao.findAll();
-		model.addObject("especialidades", espec);
-		return model;
-	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public String save(Especialidade especialidade) {
-		dao.save(especialidade);
-		return "redirect:especialidades";
-	}
+	private EspecialidadeDAO especDAO;
 	
 	@RequestMapping("/form")
-	public String form() {
-		return "especialidade/form";
+	public ModelAndView form(Especialidade especialidade) {
+		ModelAndView modelForm = new ModelAndView("especialidade/form");
+		modelForm.addObject("especialidade", especialidade);
+		return modelForm;
 	}
+	
+
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView save(Especialidade especialidade) {
+		especDAO.save(especialidade);
+		return list();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView list() {
+		ModelAndView modelList = new ModelAndView("especialidade/list");
+		List<Especialidade> especialidade = especDAO.findAll();
+		modelList.addObject("especialidades", especialidade);
+		return modelList;
+	}
+
+	@RequestMapping("/delete/{id}")
+	public ModelAndView delete( Long id) {
+		ModelAndView modelList = new ModelAndView("especialidade/list");
+		Optional<Especialidade> optionalEspecialidade = especDAO.findById(id);
+		if (optionalEspecialidade != null) {
+			modelList.addObject("deletado", "Evento deletado com sucesso!");
+			especDAO.deleteById(id);
+		}else
+			modelList.addObject("error", "Evento não encontrado!");
+		return modelList;
+	}
+	
+	@RequestMapping("/edit/{id}")
+	public ModelAndView edit(@PathVariable("id") Long id) {
+		ModelAndView modelForm = new ModelAndView("especialidade/form");
+		Optional<Especialidade> optionalEspecialidade = especDAO.findById(id);
+		if (optionalEspecialidade != null)
+			modelForm.addObject("evento", especDAO.findById(id));
+		else
+			modelForm.addObject("error", "Especialidade não encontrada!");
+		return modelForm;
+	}
+	
 }
