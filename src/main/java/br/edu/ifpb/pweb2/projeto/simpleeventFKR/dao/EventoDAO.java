44426044cdef7a39eb3b0b9d03c6b7fba0fcb801
Kamila <kmilaporto1991@gmail.com>
2019-06-04 18:44:31 -0300

    rotas funcionando

--- src/main/java/br/edu/ifpb/pweb2/projeto/simpleeventFKR/dao/EventoDAO.java
+++ src/main/java/br/edu/ifpb/pweb2/projeto/simpleeventFKR/dao/EventoDAO.java
@@ -1,7 +1,6 @@
 package br.edu.ifpb.pweb2.projeto.simpleeventFKR.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import br.edu.ifpb.pweb2.projeto.simpleeventFKR.model.Evento;
 
