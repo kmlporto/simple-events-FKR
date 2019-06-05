63374c67679e69e4f89c38241662edfdc23b1dc7
Rafael Lins <rlinsdesouza@gmail.com>
2019-06-05 01:00:40 -0300

    security control

--- src/main/java/br/edu/ifpb/pweb2/projeto/simpleeventFKR/config/SecurityWebConfig.java
+++ src/main/java/br/edu/ifpb/pweb2/projeto/simpleeventFKR/config/SecurityWebConfig.java
@@ -25,17 +25,24 @@ public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 	
 		//paginas que nao precisam de login
-		http.authorizeRequests().
-									antMatchers("/",
-//												"/usuarios",
+		http.authorizeRequests()
+									.antMatchers("/",
 												"/usuarios/**",
+												"/datafaker",
 												"/logout").permitAll()
+									.antMatchers("/especialidades/**").access("hasRole('ROLE_ADMIN')")
+//									.and().formLogin()
+////							        		.loginPage("/login")
+//							        		.defaultSuccessUrl("/")
+//							        		.failureUrl("/erro")
+//							        		.permitAll()
+									
+									
 									//necessario apenas para liberar o acesso ao banco de testes h2-console
 									.and().authorizeRequests().antMatchers("/h2-console/**").permitAll()
 									.and().csrf().ignoringAntMatchers("/h2-console/**")
 									.and().headers().frameOptions().sameOrigin();
 		
-		super.configure(http);
 	}
 	
 
@@ -44,7 +51,6 @@ public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
 	auth
 		.userDetailsService(usuarioDetalhes)
 		.passwordEncoder(new BCryptPasswordEncoder());
-		super.configure(auth);
 	}
 	
 }
--- src/main/java/br/edu/ifpb/pweb2/projeto/simpleeventFKR/controller/FakerController.java
+++ src/main/java/br/edu/ifpb/pweb2/projeto/simpleeventFKR/controller/FakerController.java
@@ -1,26 +1,35 @@
 package br.edu.ifpb.pweb2.projeto.simpleeventFKR.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.github.javafaker.Faker;
 
 import br.edu.ifpb.pweb2.projeto.simpleeventFKR.dao.EspecialidadeDAO;
+import br.edu.ifpb.pweb2.projeto.simpleeventFKR.dao.UserDAO;
 import br.edu.ifpb.pweb2.projeto.simpleeventFKR.model.Especialidade;
+import br.edu.ifpb.pweb2.projeto.simpleeventFKR.model.User;
 
 @Controller
 @RequestMapping("/datafaker")
 public class FakerController {
 //	https://github.com/DiUS/java-faker
 	Faker faker = new Faker();
+	private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	
 	@Autowired
 	private EspecialidadeDAO especialidadedao;
+	@Autowired
+	private UserDAO userdao;
+	
 	
 	@RequestMapping
 	public String createDataFaker() {
 		createDataEspecialidade();
+		createDataUser();
 		return "datafaker";
 	}
 	
@@ -29,11 +38,28 @@ public class FakerController {
 		for (int i = 0; i < 10; i++) {
 			especialidade = new Especialidade();
 			especialidade.setNome(faker.company().profession());
-			especialidade.setDescricao(faker.lorem().paragraph());
+			especialidade.setDescricao(faker.lorem().characters(100));
 			especialidadedao.save(especialidade);
 		}
 		
 	}
-
+	
+	public void createDataUser () {
+		User user;
+		user = new User();
+		user.setNome("admin");
+		user.setEmail("admin@test");
+		user.setAdmin(true);
+		user.setSenha(passwordEncoder.encode("admin"));
+		userdao.save(user);
+		for (int i = 0; i < 10; i++) {
+			user = new User();
+			user.setNome(faker.name().firstName());
+			user.setEmail(user.getNome().toLowerCase()+"@test");
+			user.setSenha(passwordEncoder.encode(user.getNome().toLowerCase()));
+			userdao.save(user);
+		}
+		
+	}
 
 }
